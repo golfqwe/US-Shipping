48223ce8d4543591095c5a52a884a4265e540ac6
sunthorn wisaidee <sunthorn.w@cdg.co.th>
2023-05-24 12:39:24 +0700

    :bug: faix api

--- pages/warehouse.vue
+++ pages/warehouse.vue
@@ -1,40 +1,61 @@
 <script lang="ts" setup>
+// import type { wareHouse } from '@/types/wareHouse/index'
 definePageMeta({
   layout: 'guest'
 })
-const tab = ref(0)
+
+const items = reactive({})
+const currentItem = ref(Object.keys(items)[0])
+
+const { data: listWarehouse } = await useFetch('/api/warehouse/', {
+  method: 'GET',
+  query: { status: 'active' }
+})
+
+const groupBy = (objectArray: [], groupKey: string) => {
+  return objectArray.sort((a, b) => {
+    if (a.country > b.country) {
+      return 1
+    }
+    if (b.country > a.country) {
+      return -1
+    }
+    return 0
+  }).reduce((acc, obj) => {
+    const key = obj[groupKey]
+    const curGroup = acc[key] ?? []
+    return { ...acc, [key]: [...curGroup, obj] }
+  }, {})
+}
+
+Object.assign(items, groupBy(listWarehouse.value as [], 'country'))
+
 </script>
 
 <template>
   <v-sheet class="pa-6" color="white" rounded>
     <v-row no-gutters>
       <v-tabs
-        v-model="tab"
+        v-model="currentItem"
         color="primary"
       >
-        <v-tab value="one">
-          Item One
-        </v-tab>
-        <v-tab value="two">
-          Item Two
-        </v-tab>
-        <v-tab value="three">
-          Item Three
+        <v-tab
+          v-for="(it,ind) in items"
+          :key="ind"
+          :value="ind"
+        >
+          {{ ind }}
         </v-tab>
       </v-tabs>
     </v-row>
     <v-container>
-      <v-window v-model="tab">
-        <v-window-item value="one">
-          One
-        </v-window-item>
-
-        <v-window-item value="two">
-          Two
-        </v-window-item>
-
-        <v-window-item value="three">
-          Three
+      <v-window v-model="currentItem">
+        <v-window-item
+          v-for="(it,ind) in items"
+          :key="ind"
+          :value="ind"
+        >
+          {{ it }}
         </v-window-item>
       </v-window>
     </v-container>
--- server/api/warehouse/index.get.ts
+++ server/api/warehouse/index.get.ts
@@ -1,14 +1,25 @@
 import { WareHouseModel } from '~/server/models/Warehouse.model'
 
 export default defineEventHandler(async (event) => {
-  const { carrier } = getQuery(event)
+  const { carrier, groupBy, status } = getQuery(event)
   let where = {}
+  let tgroupBy = ''
   if (carrier) {
     where = {
       carrier,
       status: 'active'
     }
   }
-  const users = await WareHouseModel.findAll({ where })
+  if (status) {
+    where = {
+      ...where,
+      status
+    }
+  }
+
+  if (groupBy) {
+    tgroupBy = groupBy as string
+  }
+  const users = await WareHouseModel.findAll({ where, group: tgroupBy })
   return users
 })
